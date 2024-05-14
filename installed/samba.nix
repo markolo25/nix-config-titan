a02c2cd5ae1915aebe9bd1eaaa9e819e8d1a64b6
Mark Mendoza <markolo25@gmail.com>
2024-05-13 18:20:18 -0700

    end each folder config with ;

--- installed/samba.nix
+++ installed/samba.nix
@@ -6,30 +6,30 @@ services.nas = {
     Amanda = {
         path = /mnt/athena/Amanda;
         nfs.enable = false;
-    }
+    };
     Downloads = {
         path = /mnt/athena/Downloads;
         nfs.enable = true;
-    }
+    };
     Mark = {
         path = /mnt/athena/Mark;
         nfs.enable = false;
-    }
+    };
     pwmanager = {
         path = /mnt/athena/pwmanager;
         nfs.enable = false;
-    }
+    };
     Share = {
         path = /mnt/athena/Share;
         nfs.enable = false;
-    }
+    };
     torrentclient = {
         path = /mnt/athena/torrentclient;
         nfs.enable = false;
-    }
+    };
     Video = {
         path = /mnt/athena/Video;
         nfs.enable = true;
-    }
+    };
   }
 };
