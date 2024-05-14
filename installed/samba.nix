2711bfeb170385fe149d3f9152a7d40aa791320e
Mark Mendoza <markolo25@gmail.com>
2024-05-13 22:19:16 -0700

    fix samba not working

--- installed/samba.nix
+++ installed/samba.nix
@@ -32,6 +32,8 @@
         browseable = "yes";
         "read only" = "no";
         "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
         "valid users" = "markolo25";
       };
       pwmanager = {
