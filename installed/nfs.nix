84619d2b14ae9f616203b39b9c00cf41ee3cfa38
mark mendoza <markolo25@gmail.com>
2024-06-22 22:31:28 -0700

    added nfs

---
+++ installed/nfs.nix
@@ -0,0 +1,19 @@
+{
+  services.nfs.server = {
+    enable = true;
+    # fixed rpc.statd port; for firewall
+    lockdPort = 4001;
+    mountdPort = 4002;
+    statdPort = 4000;
+    extraNfsdConfig = "";
+  };
+  networking.firewall = {
+    enable = true;
+    # for NFSv3; view with `rpcinfo -p`
+    allowedTCPPorts = [ 111 2049 4000 4001 4002 20048 ];
+    allowedUDPPorts = [ 111 2049 4000 4001 4002 20048 ];
+  };
+  services.nfs.server.exports = ''
+    /mnt/athena/Video  192.168.1.0/24(rw,sync,no_subtree_check)
+  '';
+}
